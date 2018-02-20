fc24802cae3daaf6e21077a4def8be063b9b2094
Dawid Worhacz <worhacz.dawid@gmail.com>
2018-02-20 22:46:18 +0100

    Edytowanie tekstow na podstronach 'o nas' z poziomu admina

--- fun_race/lato_o_nas.php
+++ fun_race/lato_o_nas.php
@@ -10,7 +10,7 @@
     <?php get_template_part("template/menu-lato"); ?>	
 		
 	<div class="poster-sub flex flex-justify-center flex-items-center">
-		<div class="cover"></div>
+		<div class="cover" style='background-image:url(<?php echo get_the_post_thumbnail_url( get_post()->ID, 'full' ); ?>);'></div>
         <div class="filtr"></div>
 		<div class="content">
 			<h2>Fun&amp;Race <span class="block">O naszej szkole</span></h2>
@@ -28,219 +28,45 @@
     <div class="container flex flex-column flex-row-mm flex-items-start">
         <div class="main">
            <div class="content">
-               <div class="sub-title">Kilka słów o FUN&amp;RACE</div>
-               <div class="title">… czyli poznaj nas bliżej</div>
-               <p>
-        Zamiłowanie do sportu i związane z tym lata doświadczeń, wiarygodność i pełen profesjonalizm, różnorodność propozycji – bo nie ma dla nas rzeczy niemożliwych. Firma FUN&amp;RACE to dynamiczny  zespół ludzi, dla których sport to pasja i życie. Z takim samym zaangażowaniem i entuzjazmem, który włożyliśmy niegdyś w zdobywanie naszych sportowych umiejętności, wychodzimy teraz naprzeciw oczekiwaniom naszych klientów.
-                </p>
-                <br>
-                <br>
-               <p>
-            FUN&amp;RACE od 6 lat specjalizujemy się w organizacji eventów, imprez integracyjnych, wycieczek oraz aktywnego wypoczynku na terenie Pienin i Podhala. Własna baza sprzętowa, doświadczona i wykwalifikowana kadra, współpraca wyłącznie ze sprawdzonymi obiektami noclegowymi i gastronomicznymi pozwala nam tworzyć oferty „szyte na miarę” i świadczyć usługi na najwyższym poziomie, dając tym samym naszym klientom komfort pobytu i świetną zabawę. Chcemy aby wypoczynek, czy spotkanie integracyjne było niezapomnianym przeżyciem, profesjonalnie zorganizowanym i spełniającym oczekiwania naszych klientów.
-               </p>
-                <div class="white-space-80"></div>
-                <div class="sub-title">FUN&amp;RACE team</div>
-               <div class="title">Zapraszamy do zapoznania się z naszą ofertą i do współpracy.</div>
-                <div class="white-space-80"></div>
-              
+				<div class="sub-title">Kilka słów o FUN&amp;RACE</div>
+				<div class="title">… czyli poznaj nas bliżej</div>
+				<?php echo apply_filters( 'the_content', get_post_meta( get_post()->ID, 'kilka_slow', true ) ); ?>
+				<div class="white-space-80"></div>
+				<div class="sub-title">FUN&amp;RACE team</div>
+				<div class="title">Zapraszamy do zapoznania się z naszą ofertą i do współpracy.</div>
+				<?php echo apply_filters( 'the_content', get_post_meta( get_post()->ID, 'team', true ) ); ?>
+				<div class="white-space-80"></div>
+		  
            </div>
-            <div class="wrapper flex flex-justify-center flex-justify-between-ml flex-wrap">
-                 <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">Kamil Zaród</div>
-                             <div class="stand">INSTRUKTOR, WSPÓŁWŁAŚCICIEL FIRMY</div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                              <p>Współzałożyciel FUN&RACE, mgr Akademii Wychowania Fizycznego w Krakowie, Ratownik GOPR, człowiek wielu sportowych pasji: narciarstwa, skialpinizm, kajakarstwa, raftingu. Gdy nie ma nart na nogach lub wiosła w rękach przemierza na dwóch kółkach lub nogach górskie szlaki, startując w maratonach rowerowych lub górskich biegach. Mistrz Polski w kajakarstwie górskim, Instruktor Sportu z kajakarstwa górskiego. Doświadczenie, świetne przygotowanie merytoryczne i dydaktyczne. </p>
-                               <p> W FUN&amp;RACE odpowiedzialny za organizację szkoleń kajakowych, spływów oraz obsługę wypożyczalni.</p>
-                          </div>
-                     </a>
-                 </div>
-                <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">ANNA BUCZEK</div>
-                             <div class="stand">EVENT MANAGER, WSPÓŁWŁAŚCICIEL FIRMY</div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                              <p>Współzałożyciel FUN&amp;RACE, mgr Akademii Wychowania Fizycznego w Krakowie. Potrafi łączyć pasję z pracą zawodową. Miłośniczka narciarstwa w każdym wydaniu, któremu poświęca większą część roku. Kiedy jednak kończy sezon zimowy i odkłada narty otwiera komputer i nie rozstaje się z telefonem. Koordynuje pracę całego team’u, czuwając aby wszystko było dopięte na ostatni guzik.
-                          </p>
-                               <p>   W FUN&amp;RACE odpowiedzialna za obsługę klientów grupowych i indywidualnych oraz opracowywanie scenariuszy niezapomnianych imprez..</p>
-                          </div>
-                     </a>
-                 </div>
-                <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">MAŁGORZATA BUCZEK</div>
-                             <div class="stand">EVENT MANAGER</div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                              <p></p>
-                          </div>
-                     </a>
-                 </div>
-                  <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">KONRAD GĘBALA</div>
-                             <div class="stand">Instruktor</div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                              <p>Niezastąpiony podczas rozgrywek paintballu, świetny taktyk, organizator. Posiada niezliczony zasób góralskich kawałów.
-                            </p>
-                            <p>
-                                W FUN&amp;RACE odpowiedzialny za organizację spływów pontonowych, rozgrywek paint-ball.
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                 <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">HUBERT GĘBALA</div>
-                             <div class="stand">Instruktor</div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p> Były zawodnik w kajakarstwie górskim, wielkorotny medalista Mistrzostw Polski. Jako miłośnik sportów extremalnych poszukuje adrenaliny w każdym wydaniu … najchętniej tej w postaci downhillu rowerowego. W zimie zamienia dwa kółka na jedną deskę i z taką samą pasją pracuje jako instruktor snowboardu. Jedno trzeba przyznać – jego umiejętności przebija jedynie jego poczucie humoru, którego nigdy mu nie brakuje.
-
-
-                            </p>
-                            <p>
-                                W FUN&amp;Race odpowiedzialny za obsługę spływów pontonowych i wycieczek rowerowych.
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">GRZEGORZ KWIATEK</div>
-                             <div class="stand">Instruktor</div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p> Student AWF w Krakowie, Ratownik GOPR, reprezentant Polski w zawodach w kajakarstwie górskim w kategorii C-1, medalista Mistrzostw Europy, wielokrotny Mistrz Polski. Człowiek gór, miłośnik sportu i rywalizacji. Kiedy nie pływa na kajaku, jedździ na rowerze lub biega, ścigając się w maratonach rowerowych lub górskich maratonach biegowych. Kiedy na szlakach robi się tłoczno zabiera sprzęt wspinaczkowy a zimą narty turowe. Sport porzuca tylko w jednym wypadku, kiedy na wezwanie Naczelnika GOPR trzeba nieść pomoc w górach. W FUN&amp;RACE odpowiedzialny za organizację spływów pontonowych, kajakowych oraz zajęć linowych. 
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                  <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">MICHAŁ KUBIK</div>
-                             <div class="stand"></div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p>
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">Mateusz LIZOŃ</div>
-                             <div class="stand"></div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p>
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                  <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">Adam Olędzki</div>
-                             <div class="stand"></div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p>
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                  <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">Artur Wojtaszek</div>
-                             <div class="stand"></div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p>
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-                  <div class="element">
-                     <a class="inner"> 
-                         <div class="cover"></div>
-                         <div class="label">
-                             <div class="name">Radek Wojtaszek</div>
-                             <div class="stand"></div>
-                             <div class="flex more flex-items-center">
-                                 <div class="text">czyaj dalej</div>
-                                 <div class="rotate"><div class="arrow"></div></div>
-                             </div>
-                         </div>
-                          <div class="about">
-                             <p>
-                            </p>
-                          </div>
-                     </a>
-                 </div>
-            </div>
+           <div class="wrapper flex flex-justify-center flex-justify-between-ml flex-wrap">
+			<?php
+				$pages = get_pages( array(
+					'parent' => 264,
+					'sort_order' => 'ASC',
+					'sort_column' => 'menu_order, post_title',
+					
+				) );
+				
+				foreach( $pages as $item ):
+			?>
+				<div class="element">
+					<a class="inner"> 
+					<div class="cover" style='background-image:url(<?php echo get_the_post_thumbnail_url( $item->ID, 'full' ); ?>);'></div>
+					<div class="label">
+						<div class="name"><?php echo $item->post_title; ?></div>
+						<div class="stand"><?php echo get_post_meta( $item->ID, 'rank', true ); ?></div>
+						<div class="flex more flex-items-center">
+							<div class="text">czyaj dalej</div>
+							<div class="rotate"><div class="arrow"></div></div>
+						</div>
+					</div>
+						<div class="about">
+							<?php echo apply_filters( 'the_content', $item->post_content ); ?>
+						</div>
+					</a>
+				</div>
+                <?php endforeach; ?>
+			</div>
         </div>
     </div>
 </div>
